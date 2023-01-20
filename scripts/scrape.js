a75a8b10b71f3d4d208b828a91fcc10a630d3237
John Lindquist <johnlindquist@gmail.com>
2023-01-20 15:36:31 -0700

    fix(init): go

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -3,12 +3,8 @@ let selector = await arg("Enter selector")
 
 let results = await scrapeSelector(url, selector)
 
-console.log({ results })
-
 // Rest of this script is for uploading the data to GitHub releases
 let octokit = github.getOctokit(await env("GITHUB_TOKEN"))
-
-console.log({ octokit })
 
 let { format } = await npm("date-fns")
 let dateTag = format(new Date(), "yyyy-MM-dd-HH-mm")
@@ -16,14 +12,10 @@ let { host } = new URL(url)
 
 let name = `${host}-${dateTag}.json`
 
-console.log({ name })
-
 let releaseResponse = await octokit.rest.repos.createRelease({
   ...github.context.repo,
   tag_name: name,
 })
-
-console.log({ releaseResponse: releaseResponse.data })
 
 let headers = {
   "content-type": "image/png",
@@ -36,7 +28,3 @@ let uploadResponse = await octokit.rest.repos.uploadReleaseAsset({
   name,
   data: Buffer.from(JSON.stringify(results)),
 })
-
-console.log({ uploadResponse: uploadResponse.data })
-
-console.log(`url: ${uploadResponse.data.browser_download_url}`)
