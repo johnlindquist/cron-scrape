a6e7ca6ec9421adc9f530a48dc0f989841c76b76
John Lindquist <johnlindquist@gmail.com>
2023-01-20 14:48:18 -0700

    fix(init): go

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -6,7 +6,8 @@ let data = await scrapeSelector(url, selector)
 // Rest of this script is for uploading the data to GitHub releases
 let octokit = github.getOctokit(await env("GITHUB_TOKEN"))
 
-let dateTag = formatDate(new Date(), "yyyy-MM-dd-HH-mm")
+let { format } = await npm("date-fns")
+let dateTag = format(new Date(), "yyyy-MM-dd-HH-mm")
 
 let releaseResponse = await octokit.rest.repos.createRelease({
   ...github.context.repo,
